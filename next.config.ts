ad8becab489307debe48e7c8e50047be39867634
Samarth Malhotra <malhotra.samarth@hotmail.com>
2025-11-08 21:59:00 +0530

    adding changes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
+  ...(isGithubPages && {
+    basePath: "/evenup-web",
+    assetPrefix: "/evenup-web",
+  }),
   images: {
     unoptimized: true,
   },
