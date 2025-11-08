aaf91c9250d0b8436184a6198a83e95b2cc48b3f
Samarth Malhotra <malhotra.samarth@hotmail.com>
2025-11-08 22:05:58 +0530

    update configurations

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
-// next.config.ts
 import type { NextConfig } from "next";
-
-const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
-  ...(isGithubPages && {
-    basePath: "/evenup-web",
-    assetPrefix: "/evenup-web",
-  }),
   images: {
     unoptimized: true,
   },
+  // Don't use basePath for custom domain
+  trailingSlash: true,
 };
 
 export default nextConfig;
